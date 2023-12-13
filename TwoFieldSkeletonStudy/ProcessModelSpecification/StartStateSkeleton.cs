0756080ebdf7bd63bd331cc525a719f9124f0728
Benedict Harder <benedict.harder@tum.de>
2023-12-13 15:50:17 +0100

    added angle calculation to recursive targets

--- TwoFieldSkeletonStudy/ProcessModelSpecification/StartStateSkeleton.cs
+++ TwoFieldSkeletonStudy/ProcessModelSpecification/StartStateSkeleton.cs
@@ -25,8 +25,7 @@ namespace TwoFieldSkeletonStudy
         {
             //get edges of rectangular plot
             Rectangle3d auxiliaryGeometry = (Rectangle3d)AuxiliaryGeometry;
-            List<Vector3d> targetsForFoundations = new List<Vector3d>(); 
-            List<double> rotationAnglesForFoundations = new List<double>();
+            List<Tuple<Vector3d, double>> targetsForFoundations = new List<Tuple<Vector3d, double>>();
             int nrOfFields = (int)customisationSettings.Parameters["NumberOfFields"];
             double zcoord = customisationSettings.Parameters["NumberOfBasementStoreys"] * customisationSettings.Parameters["StoreyHeight"] * (-1);
 
@@ -36,56 +35,82 @@ namespace TwoFieldSkeletonStudy
             int j = 0;
             char rotationAxis = 'z';
             List<Component> startSymbol = new List<Component>();
-            foreach (Vector3d target in targetsForFoundations)
+            foreach (Tuple<Vector3d, double> target in targetsForFoundations)
             {
                 Component foundation = RewritingHandler.CreateComponentInGlobalOrigin(this.StartPart);
-                //foundation.RotateAroundGlobalOrigin(rotationAnglesForFoundations.ElementAt(j), rotationAxis); //first rotate around origin, then translate (order is important!)
-                foundation.Translate(target);
+                foundation.RotateAroundGlobalOrigin(target.Item2, rotationAxis); //first rotate around origin, then translate (order is important!)
+                foundation.Translate(target.Item1);
                 startSymbol.Add(foundation);
                 j++;
             }
             return startSymbol;
         }
 
-        public List<Vector3d> recursiveTargets(Rectangle3d rect, int nrOfFields, double zcoord)
+        public List<Tuple<Vector3d, double>> recursiveTargets(Rectangle3d rect, int nrOfFields, double zcoord)
         {
-            List<Vector3d> targetsForFoundations = new List<Vector3d>();
-            if (nrOfFields <= 0)
+            List<Tuple<Vector3d, double>> targetsForFoundations = new List<Tuple<Vector3d, double>>();
+
+            if (nrOfFields == 0)
             {
-                targetsForFoundations.Add(new Vector3d(rect.X.Mid, rect.Y.Mid, zcoord));
+                Vector3d vec = new Vector3d(rect.X.Mid, rect.Y.Mid, zcoord);
+                double angle = 0.0;
+                targetsForFoundations.Add(Tuple.Create(vec, angle));
                 return targetsForFoundations;
+            }
+            else if (nrOfFields < 0)
+            {
+                return new List<Tuple<Vector3d, double>>();
             }
             double xSection = GetSection(nrOfFields, 'x', rect);
             double ySection = GetSection(nrOfFields, 'y', rect);
 
             int k = 0;
             int l = 0;
+            int p = 0;
+            int r = 0;
             double xcoord = rect.X.T0;
             double ycoord = rect.Y.T0;
             bool rec = false;
             while (!rec)
             {
+                if (p == nrOfFields - 1)
+                {
+                    p = 0;
+                    r += 1;
+                }
+                else
+                {
+                    p += 1;
+                }
                 if (k <= nrOfFields - 1 && l == 0)
                 {
-                    targetsForFoundations.Add(new Vector3d(xcoord, ycoord, zcoord));
+                    Vector3d vec = new Vector3d(xcoord, ycoord, zcoord);
+                    double angle = r * 0.5 * Math.PI;
+                    targetsForFoundations.Add(Tuple.Create(vec, angle));
                     xcoord += xSection;
                     k++;
                 }
                 else if (k > nrOfFields - 1 && l <= nrOfFields - 1)
                 {
-                    targetsForFoundations.Add(new Vector3d(xcoord, ycoord, zcoord));
+                    Vector3d vec = new Vector3d(xcoord, ycoord, zcoord);
+                    double angle = r * 0.5 * Math.PI;
+                    targetsForFoundations.Add(Tuple.Create(vec, angle));
                     ycoord += ySection;
                     l++;
                 }
                 else if (k <= 2 * nrOfFields - 1 && l > nrOfFields - 1)
                 {
-                    targetsForFoundations.Add(new Vector3d(xcoord, ycoord, zcoord));
+                    Vector3d vec = new Vector3d(xcoord, ycoord, zcoord);
+                    double angle = r * 0.5 * Math.PI;
+                    targetsForFoundations.Add(Tuple.Create(vec, angle));
                     xcoord -= xSection;
                     k++;
                 }
                 else if (k > 2 * nrOfFields - 1 && l <= 2 * nrOfFields - 1)
                 {
-                    targetsForFoundations.Add(new Vector3d(xcoord, ycoord, zcoord));
+                    Vector3d vec = new Vector3d(xcoord, ycoord, zcoord);
+                    double angle = r * 0.5 * Math.PI;
+                    targetsForFoundations.Add(Tuple.Create(vec, angle));
                     ycoord -= ySection;
                     l++;
                 }
