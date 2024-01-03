bf342ac93d5f5e6eddabf642179952b45f914cf6
Benedict Harder <benedict.harder@tum.de>
2024-01-03 13:18:12 +0100

    comments for start state skeleton

--- TwoFieldSkeletonStudy/ProcessModelSpecification/StartStateSkeleton.cs
+++ TwoFieldSkeletonStudy/ProcessModelSpecification/StartStateSkeleton.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -46,12 +47,23 @@ namespace TwoFieldSkeletonStudy
             return startSymbol;
         }
 
+        /// <summary>
+        /// this recursive method calculated the coordinated and rotation angles for the foundations. It is applicable for any symmetric configaration
+        /// Basically, we always start with the outer targets in ccw direction, and the recursively invoke the method again, but with a smaller rectangle which is located inside the old/bigger one
+        /// </summary>
+        /// <param name="rect"></param> The plot
+        /// <param name="nrOfFields"></param> the number of fields you want per side
+        /// <param name="zcoord"></param> the z height (static)
+        /// <returns></returns> a list of tuples, which contain vectors and angles
         public List<Tuple<Vector3d, double>> recursiveTargets(Rectangle3d rect, int nrOfFields, double zcoord)
         {
+            // declare the return list
             List<Tuple<Vector3d, double>> targetsForFoundations = new List<Tuple<Vector3d, double>>();
 
+            // this is our exit condition
             if (nrOfFields == 0)
             {
+                // if it is excatly 0, we need to add a tagret right in the middle
                 Vector3d vec = new Vector3d(rect.X.Mid, rect.Y.Mid, zcoord);
                 double angle = 0.0;
                 targetsForFoundations.Add(Tuple.Create(vec, angle));
@@ -59,21 +71,26 @@ namespace TwoFieldSkeletonStudy
             }
             else if (nrOfFields < 0)
             {
+                // otherwise just return an empty list
                 return new List<Tuple<Vector3d, double>>();
             }
+            // retrieve the section lengths, depending on the nr of fields specified
             double xSection = GetSection(nrOfFields, 'x', rect);
             double ySection = GetSection(nrOfFields, 'y', rect);
 
+            // init counting variables
             int k = 0;
             int l = 0;
             int p = 0;
             int r = 0;
+            // retrieve the rectangle origin
             double xcoord = rect.X.T0;
             double ycoord = rect.Y.T0;
             bool rec = false;
             while (!rec)
             {
-                
+                // here we check for certain conditions which define the target coordinated and the rotation angle
+                // in summary we count the number of targets that we have placed and then calculate the coords and angles
                 if (k <= nrOfFields - 1 && l == 0)
                 {
                     Vector3d vec = new Vector3d(xcoord, ycoord, zcoord);
@@ -108,6 +125,7 @@ namespace TwoFieldSkeletonStudy
                 }
                 else
                 {
+                    // if none of the above conditions are met, we invoke the method again
                     double singleXSection = GetSection(nrOfFields, 'x', rect);
                     double singleYSection = GetSection(nrOfFields, 'y', rect);
                     Rectangle3d newRect = new Rectangle3d(Plane.WorldXY, new Point3d(rect.X.T0 + singleXSection, rect.Y.T0 + singleYSection, zcoord), new Point3d(rect.X.T1 - singleXSection, rect.Y.T1 - singleYSection, zcoord));
@@ -127,6 +145,14 @@ namespace TwoFieldSkeletonStudy
             return targetsForFoundations;
         }
 
+        /// <summary>
+        /// This method divides a rectangle into equidistant sections
+        /// </summary>
+        /// <param name="nrOfFields"></param>
+        /// <param name="axis"></param>
+        /// <param name="geom"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public double GetSection(int nrOfFields, char axis, Rectangle3d geom)
         {
             if (axis == 'x')
